8c6ae038dd8ccb3dbc9999507e5cff5d99ff1886
Bellal Mohamed <mbellal2000@gmail.com>
2018-01-09 04:11:54 +0200

    Removed total_likes column

--- blat/admin.py
+++ blat/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from blat.models import Blat
 # Register your models here.
 class BlatAdmin(admin.ModelAdmin):
-	list_display = ['text', 'via', 'created_on', 'total_likes']
+	list_display = ['text', 'via', 'created_on']
 	list_filter = ['created_on']
 	search_fields = ['text']
 
